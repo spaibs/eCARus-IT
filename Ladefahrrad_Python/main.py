519fbad27f851303b2d09f5b685aed6c4bec97e6
Fabian Graf <fabian.graf@outlook.de>
2015-09-07 12:03:28 +0200

    change ip-address of receiver

--- Ladefahrrad_Python/main.py
+++ Ladefahrrad_Python/main.py
@@ -1,6 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.uic import *
+
 __author__ = 'Fabian Graf'
 
 
-class Main(object):
+class Main(QMainWindow):
     def __init__(self):
-        pass
+        QMainWindow.__init__(self)
+        self.ui = loadUi('X:/projects/2010/python/pyqt_tutorial/pyqt_tutorial.ui')
+        self.ui.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
