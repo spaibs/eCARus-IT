28336bfe08a09a213df262e1a22587e5b9942914
Fabian Graf <fabian.graf@outlook.de>
2015-09-11 14:21:39 +0200

    new folder structure

--- GUI_Fahrzeug/GUI/app/src/main/java/de/tum/ei/ecarus/gui/MainActivity.java
+++ GUI_Fahrzeug/GUI/app/src/main/java/de/tum/ei/ecarus/gui/MainActivity.java
@@ -1,11 +1,11 @@
 package de.tum.ei.ecarus.gui;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
