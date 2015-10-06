ad41ebc9be6eb23fcba44d3a084614beb5081696
jonasstabenow <jonasstabenow@hotmail.de>
2015-10-06 14:49:17 +0200

    added java app working for EB version 6

---
+++ GUI_Fahrzeug/Fahrzeug_eCARus_Auto_Java_6/app/src/main/java/com/example/gtflauncher/tower/TowerMessageProcessor.java
@@ -0,0 +1,8 @@
+package com.example.gtflauncher.tower;
+
+import com.example.gtflauncher.tower.TowerMessage;
+
+public interface TowerMessageProcessor
+{
+    void processTowerMessage(final TowerMessage message);
+}
---
+++ GUI_Fahrzeug/Fahrzeug_eCARus_Auto_Java_6/app/src/main/java/com/example/gtflauncher/tower/TowerReceiver.java
@@ -0,0 +1,68 @@
+package com.example.gtflauncher.tower;
+
+import android.util.Log;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class TowerReceiver implements Runnable
+{
+    private final static String LOG_TAG = "TowerReceiver";
+    private final static int TOWER_PORT = 32787;
+
+    private Thread listenerThread;
+    private DatagramSocket listenerSocket;
+    private TowerMessageProcessor processor;
+
+    public TowerReceiver(TowerMessageProcessor processor)
+    {
+        if(processor == null)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        this.processor = processor;
+
+        listenerThread = new Thread(this);
+        listenerThread.start();
+    }
+
+    public void shutdown()
+    {
+        if(listenerThread != null)
+        {
+            Log.d(LOG_TAG, "Shutting down tower listener thread...");
+            listenerThread.interrupt();
+        }
+    }
+
+    @Override
+    public void run()
+    {
+        byte[] messageBuffer = new byte[255];
+        DatagramPacket messagePacket = new DatagramPacket(messageBuffer, messageBuffer.length);
+
+        Log.d(LOG_TAG, "Tower listener thread started. Waiting for connection...");
+
+        try
+        {
+            listenerSocket = new DatagramSocket(TOWER_PORT);
+            while(!listenerThread.isInterrupted())
+            {
+                listenerSocket.receive(messagePacket);
+
+                TowerMessage receivedMessage = TowerMessageFactory.parse(messagePacket.getData());
+
+                processor.processTowerMessage(receivedMessage);
+            }
+        }
+        catch(Exception ex)
+        {
+            if(!(ex instanceof InterruptedException))
+            {
+                ex.printStackTrace();
+                listenerThread.interrupt();
+            }
+        }
+    }
+}
