158f559dec8eae7835e95275b302957c5867e241
laurahauenschild <lauraha07@gmail.com>
2016-03-18 13:54:20 +0100

    test gui

--- eCARus Android App/eCARus/app/src/main/java/de/tum/ei/ecarus/ecarus/MainActivity.java
+++ eCARus Android App/eCARus/app/src/main/java/de/tum/ei/ecarus/ecarus/MainActivity.java
@@ -1,10 +1,5 @@
 package de.tum.ei.ecarus.ecarus;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -19,8 +14,6 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
