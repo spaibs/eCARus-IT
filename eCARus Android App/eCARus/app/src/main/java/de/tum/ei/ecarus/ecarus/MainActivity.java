7ddfb6be772c13d65de08b21db8ff4749afcde19
laurahauenschild <lauraha07@gmail.com>
2016-03-23 15:04:22 +0100

    punkte

--- eCARus Android App/eCARus/app/src/main/java/de/tum/ei/ecarus/ecarus/MainActivity.java
+++ eCARus Android App/eCARus/app/src/main/java/de/tum/ei/ecarus/ecarus/MainActivity.java
@@ -293,7 +293,7 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    // This function receives interpreted data stored in the string array data.
+    // This function receives interpreted data stored in the string array data.....
     // It is called by the UDP thread class.
     // This way the MainActivity gets access to the received information...
     public void setData(String[][] data)
