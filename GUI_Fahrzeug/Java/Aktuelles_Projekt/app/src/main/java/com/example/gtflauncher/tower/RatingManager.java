3b8362cf7b43344842715058bcb7569e5aeb4178
christian_jan <christian_j@live.de>
2016-03-31 14:13:21 +0200

    GUI Java: Final Version 15/16

---
+++ GUI_Fahrzeug/Java/Aktuelles_Projekt/app/src/main/java/com/example/gtflauncher/tower/RatingManager.java
@@ -0,0 +1,21 @@
+package com.example.gtflauncher.tower;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RatingManager extends Activity{
+
+
+    // Hierhin sollten alle Funktionen, die das Rating betreffen verlegt werden.
+
+}
--- GUI_Fahrzeug/Java/Aktuelles_Projekt/app/src/main/java/com/example/gtflauncher/tower/TowerJNIAccess.java
+++ GUI_Fahrzeug/Java/Aktuelles_Projekt/app/src/main/java/com/example/gtflauncher/tower/TowerJNIAccess.java
@@ -1,6 +1,15 @@
 package com.example.gtflauncher.tower;
 
+import android.os.Environment;
 import android.util.Log;
+
+import com.example.gtflauncher.navigation.NavigationService;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TowerJNIAccess
 {
@@ -12,6 +21,7 @@ public class TowerJNIAccess
     // JNI Methods: Java --> C++
     public static native void onLightStateUpdated(boolean lowBeamIsOn, boolean highBeamIsOn, boolean indicatorRightIsOn, boolean indicatorLeftIsOn, boolean indicatorWarningLightIsOn);
     public static native void onGestureDetected(int gestureType);
+    public static native void onAverageRating(float ratingAverage);
 
     // JNI Methods: C++ --> Java
     public static void onSetVehicleMode(int modeToSet)
@@ -26,13 +36,111 @@ public class TowerJNIAccess
         // TODO?
     }
 
+    public static void onTransmitRating(int ratingValue)
+    {
+        Log.d(TowerReceiver.LOG_TAG, "User gave a star-rating of:" + ratingValue);
+        //final RatingManager service = RatingManager.getInstance();
+        //service.storeRating(ratingValue);
+
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(path, "RatingList.txt");
+
+        if(isExternalStorageWritable()) {
+            if (!path.exists()) path.mkdirs();
+            try {
+                FileWriter writer = new FileWriter(file, true);
+                writer.append(String.valueOf(ratingValue) + "\n");
+                writer.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+                Log.d(TowerJNIAccess.class.getName(), "Problem with FileWriter.");
+            }
+
+            //Log.d(TowerJNIAccess.class.getName(), "The file is located in " + file);
+
+            //Log.d(TowerJNIAccess.class.getName(), "Data was perfectly written.");
+
+            //Log.d(TowerJNIAccess.class.getName(), "External storage was writable.");
+        }
+        else {
+            Log.d(TowerJNIAccess.class.getName(), "No access to SD card.");
+        }
+        Log.d(TowerJNIAccess.class.getName(), "Average rating of " + loadRating());
+
+    }
+
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    public static float loadRating() {
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(path, "RatingList.txt");
+
+        if(isExternalStorageWritable()) {
+            if(path.exists()) {
+                try {
+                    int sum = 0, number = 0;
+                    String line;
+
+                    BufferedReader buf = new BufferedReader(new FileReader(file));
+
+                    //Log.d(TowerJNIAccess.class.getName(),"Current line: " + buf.readLine());
+                    while((line = buf.readLine()) !=  null && line.length() > 0) {
+                        number++;
+                        sum += Integer.parseInt(line);
+                        //Log.d(TowerJNIAccess.class.getName(), "Data was read.");
+                    }
+                    buf.close();
+
+                    if(number == 0) {
+                        Log.d(TowerJNIAccess.class.getName(), "No ratings available.");
+                        return -1;
+                    } else {
+                        //Log.d(TowerJNIAccess.class.getName(),"Anzahl" + number);
+                        //Log.d(TowerJNIAccess.class.getName(),"Summe" + sum);
+                        return (float) sum/number;
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return -1;
+                }
+            }
+            return -1;
+        }
+        else {
+            Log.d(TowerJNIAccess.class.getName(), "No access to SD card.");
+            return -1;
+        }
+    }
+
+    public void clearRating() {
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(path, "RatingList.txt");
+
+        if(isExternalStorageWritable()) {
+            if(!path.exists()) path.mkdirs();
+            try {
+                FileWriter writer = new FileWriter(file);
+                writer.append("");
+                writer.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     public static void onSetLedMode(int ledModeSet)
     {
         // TODO Logik
         TowerSender towersender = null;
 
         String message = "";
-
+        //Hier koennte die zu sendende Nachricht unterschieden werden.
+        /*
         switch(ledModeSet) {
             case 1: message = "00880FF1234";
                 break;
@@ -43,7 +151,7 @@ public class TowerJNIAccess
             case 4: message = "00880FF1234";
                 break;
             default: break;
-        }
+        }*/
 
 
         // send update message
